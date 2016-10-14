589505095cfc4b87ae783d9920aedb399cc10c2d
Björn Roberg <bjorn.roberg@gmail.com>
2016-10-14 14:54:19 +0200

    Continued implementation based on @jekrb's code (#4)
    * begin cache function
    
    * clean up for linting
    
    * clean up for linting
    
    * adds initial implementation based on jekrb's code. Adds tests that checks memoization is working as expected

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const assert = require('assert')
+'use strict'
 
+const assert = require('assert')
 module.exports = cacheElement
 
-// Memoize a bel element
-// null -> null
-function cacheElement () {
+const __slice = Function.prototype.call.bind(Array.prototype.slice)
+
+function cacheElement (fn) {
+  assert(typeof fn === 'function', 'cacheElement accepts one argument, which must be a `function`')
+  const store = {}
+
+  return function render () {
+    const args = __slice(arguments, 0)
+    let argsAreTheSame = !!store.prev
+    for (let i = 0; i < (store.prev || []).length; ++i) {
+      // not `break`ing cause I don't think users will pass a million args
+      argsAreTheSame = argsAreTheSame && store.prev[i] === args[i]
+    }
+
+    if (argsAreTheSame) return store.el
+
+    store.prev = args
+    store.el = fn.apply(this, args)
+    return store.el
+  }
 }
--- test.js
+++ test.js
@@ -1,6 +1,26 @@
 const test = require('tape')
 const cacheElement = require('./')
 
+test('cache', function (t) {
+  t.plan(7)
+
+  // ensure memoization
+  var callCount = 0
+  var render = cacheElement(function (el) {
+    callCount += 1
+    return el
+  })
+
+  t.equals(callCount, 0)
+  t.same(render('alice'), 'alice')
+  t.equals(callCount, 1)
+  t.same(render('alice'), 'alice')
+  t.equals(callCount, 1)
+
+  t.same(render('bob'), 'bob')
+  t.equals(callCount, 2)
+})
+
 test('should assert input types', function (t) {
   t.plan(1)
   t.throws(cacheElement)
