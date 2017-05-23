2c3cffd3b81f87b5b1837ec593b139bf6aa216d3
Julian Gruber <julian@juliangruber.com>
2017-05-23 16:30:02 +0200

    move over props handling from microcomponent (#34)
    * move over props handling from microcomponent
    
    * add missing .oldProps initializer

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ var INDEX = 0
 
 module.exports = Nanocomponent
 
-function Nanocomponent (val) {
+function Nanocomponent () {
   this._hasWindow = typeof window !== 'undefined'
   this._ID = KEY + '-' + INDEX++
   this._placeholder = null
   this._onload = onload
   this._element = null
   this._loaded = false
+  this.props = {}
+  this.oldProps = null
+  this.state = {}
 }
 
 Nanocomponent.prototype._ensureID = function () {
@@ -22,7 +25,14 @@ Nanocomponent.prototype._ensureID = function () {
   else this._ID = id
 }
 
-Nanocomponent.prototype.render = function () {
+Nanocomponent.prototype._rerender = function (props) {
+  this.oldProps = this.props
+  this.props = props
+  this._element = this._render()
+  this._ensureID()
+}
+
+Nanocomponent.prototype.render = function (props) {
   assert.equal(typeof this._render, 'function', 'nanocomponent: this._render should be implemented')
   assert.equal(typeof this._update, 'function', 'nanocomponent: this._update should be implemented')
 
@@ -32,12 +42,10 @@ Nanocomponent.prototype.render = function () {
   for (var i = 0; i < len; i++) args[i] = arguments[i]
 
   if (!this._hasWindow) {
-    this._element = this._render.apply(this, args)
+    this._rerender(props)
     return this._element
   } else if (!this._element) {
-    this._element = this._render.apply(this, args)
-    this._ensureID()
-
+    this._rerender(props)
     this._onload(this._element, function () {
       self._loaded = true
       if (self._load) {
@@ -57,10 +65,9 @@ Nanocomponent.prototype.render = function () {
     })
     return this._element
   } else {
-    var shouldUpdate = this._update.apply(this, args)
+    var shouldUpdate = this._update(props)
     if (shouldUpdate) {
-      this._element = this._render.apply(this, args)
-      this._ensureID()
+      this._rerender(props)
     }
     if (!this._placeholder) this._placeholder = this._createPlaceholder()
     return this._placeholder
