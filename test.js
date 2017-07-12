7bb3d9eee2b8dd9ffe2735f457cf65f37b249ad2
Bret Comnes <bcomnes@gmail.com>
2017-07-11 21:54:15 -0700

    Fix server side tests

--- test.js
+++ test.js
@@ -1,67 +1,33 @@
-var CacheComponent = require('./')
+var Nanocomponent = require('./')
 var test = require('tape')
 var html = require('bel')
 
 test('cache', (t) => {
   t.test('should validate input types', (t) => {
     t.plan(1)
-    var comp = new CacheComponent()
+    var comp = new Nanocomponent()
     t.throws(comp.render.bind(comp), /_render should be implemented/)
   })
 
-  t.skip('should render elements', (t) => {
-    t.plan(3)
+  t.test('should render elements', (t) => {
+    t.plan(2)
 
     function MyComp () {
       if (!(this instanceof MyComp)) return new MyComp()
-      CacheComponent.call(this)
+      Nanocomponent.call(this)
     }
-    MyComp.prototype = Object.create(CacheComponent.prototype)
+    MyComp.prototype = Object.create(Nanocomponent.prototype)
 
     MyComp.prototype._render = function (name) {
-      return html`
-        <div>${name}</div>
-      `
+      return html`<div>${name}</div>`
     }
 
     var myComp = new MyComp()
 
     var el1 = myComp.render('mittens')
     t.equal(String(el1), '<div>mittens</div>', 'init render success')
-
-    var el2 = myComp.render('mittens')
-    var same1 = el2.isSameNode(el1)
-    t.equal(same1, true, 'proxy success')
 
     var el3 = myComp.render('scruffles')
     t.equal(String(el3), '<div>scruffles</div>', 're-render success')
-  })
-
-  t.skip('should accept a custom compare function', (t) => {
-    t.plan(2)
-    function MyComp () {
-      if (!(this instanceof MyComp)) return new MyComp()
-      CacheComponent.call(this)
-    }
-    MyComp.prototype = Object.create(CacheComponent.prototype)
-
-    MyComp.prototype._render = function (name) {
-      return html`
-        <div>${name}</div>
-      `
-    }
-
-    MyComp.prototype._update = function (arg1) {
-      return arg1 !== 'humans!'
-    }
-
-    var myComp = new MyComp()
-
-    var el1 = myComp.render('mittens')
-    t.equal(String(el1), '<div>mittens</div>', 'init render success')
-
-    var el2 = myComp.render('humans!')
-    var same1 = el2.isSameNode(el1)
-    t.equal(same1, true, 'proxy success')
   })
 })
